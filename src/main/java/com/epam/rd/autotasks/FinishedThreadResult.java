ff83c65db521cf89a628abef08ed33a177cb20a6
borrowsorrow <51077910+borrowsorrow@users.noreply.github.com>
2022-04-12 23:29:21 +0300

    completing task, excepts results() method

--- src/main/java/com/epam/rd/autotasks/FinishedThreadResult.java
+++ src/main/java/com/epam/rd/autotasks/FinishedThreadResult.java
@@ -1,11 +1,20 @@
 package com.epam.rd.autotasks;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.FutureTask;
+import java.util.stream.Collectors;
 
-public class FinishedThreadResult {
+public class FinishedThreadResult implements ThreadUnion{
     private final String threadName;
     private final LocalDateTime finished;
     private final Throwable throwable;
+    private int size;
+    private boolean isShutdown = false;
+    private List<Thread> threadList = new ArrayList<>();
+    private List<FinishedThreadResult> finishedThreadList = new ArrayList<>();
+    private static final String THREAD_TITLE = "testThreadName-worker-";
 
     public FinishedThreadResult(final String threadName) {
         this(threadName, null);
@@ -28,4 +37,59 @@ public class FinishedThreadResult {
     public Throwable getThrowable() {
         return throwable;
     }
+
+    @Override
+    public int totalSize() {
+        return threadList.size();
+    }
+
+    @Override
+    public int activeSize() {
+       return (int) threadList.stream().filter(Thread::isAlive).count();
+    }
+
+    @Override
+    public void shutdown() {
+        threadList.forEach(Thread::interrupt);
+        isShutdown = true;
+    }
+
+
+    @Override
+    public boolean isShutdown() {
+        return isShutdown;
+    }
+
+    @Override
+    public synchronized void awaitTermination() {
+        for (Thread thread : threadList) {
+            try {
+                finishedThreadList.add(new FinishedThreadResult(thread.getName()));
+            } catch (Throwable e) {
+                finishedThreadList.add(new FinishedThreadResult(thread.getName(), e));
+            }
+        }
+    }
+
+    @Override
+    public boolean isFinished() {
+        return isShutdown;
+    }
+
+    @Override
+    public synchronized List<FinishedThreadResult> results() {
+        return finishedThreadList;
+    }
+    @Override
+    public synchronized Thread newThread(Runnable r) {
+        if (!isShutdown) {
+            Thread thread = new Thread(r, THREAD_TITLE +  threadList.size());
+            threadList.add(thread);
+            return thread;
+        }
+        else {
+            throw new IllegalStateException();
+        }
+    }
+
 }
